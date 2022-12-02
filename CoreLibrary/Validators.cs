7eba66811a356763572c128f183744ed977eaf8c
A00259393-Vamsi <A00259393@mycambrian.ca>
2022-12-01 23:21:53 -0500

    ExpenseTracker: Added Core Library for all operations Added a validator class to add validations Added a validator method for password which validation some minimum conditions required for handling password

---
+++ CoreLibrary/Validators.cs
@@ -0,0 +1,34 @@
+﻿namespace CoreLibrary
+{
+    public class Validators
+    {
+        public static bool IsPasswordValid(string value)
+        {
+            if (value != null)
+            {
+                String password = value.ToString();
+                if (password.Length < 6 || password.Length > 20)
+                    return false;
+
+                if (!password.Any(char.IsUpper))
+                    return false;
+
+                if (!password.Any(char.IsLower))
+                    return false;
+
+                if (password.Contains(' '))
+                    return false;
+
+                string specialCharacters = @"%!@#$%^&*()?/>.<,:;'\|}]{[_~`+=-" + "\"";
+                char[] specialCharacterArray = specialCharacters.ToCharArray();
+                foreach (char ch in specialCharacterArray)
+                {
+                    if (password.Contains(ch))
+                        return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
