ac9424ce0038216b2bc33455a312a09808c228ee
A00259393-Vamsi <A00259393@mycambrian.ca>
2022-12-01 23:01:15 -0500

    ExpenseTracker: Added chnges for the expense calculate screen Navigation to the screen happens only if the login fields are filled with some values Need to still add email and password validations with the database

---
+++ ExpenseTracker/Controllers/CalculateController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseTracker.Controllers
+{
+    public class CalculateController : Controller
+    {
+        private readonly ILogger<CalculateController> _logger;
+
+        public CalculateController(ILogger<CalculateController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult? Index(string username, string password)
+        {
+            if (username != null && password != null)
+            {
+                return View();
+            }
+            return View("../Home/Index");
+        }
+    }
+}
